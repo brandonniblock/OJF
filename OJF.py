0bad987322e731d8868f5b510f711dfe506bd2a1
BrandonJohnBobNiblock <thenibsman@gmail.com>
2020-06-02 16:31:08 -0700

    sends the content of the file

--- OJF.py
+++ OJF.py
@@ -8,30 +8,27 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.message import EmailMessage
 import sys
 
 class OJF:
     def __init__(self):
-        self.subject = "Weekly Job Report"
+        self.subject = "Daily Job Report"
         self.body = "This is an email with the desired report attached"
         self.sender_email = "thenibsman@gmail.com"
         self.receiver_email = "bniblock@zagmail.gonzaga.edu"
-        self.file = "report.pdf" # in the same directory as script
+        self.file = "report.txt" # in the same directory as script
         f = open("email.ps", "r")
         password = f.read()
         self.password = password
     def sendEmail(self):
-        email = MIMEMultipart()
+        email = EmailMessage()
         email["From"] = self.sender_email
         email["To"] = self.receiver_email 
         email["Subject"] = self.subject
-        attach_file = open(self.file, "rb") # open the file
-        report = MIMEBase("application", "octate-stream")
-        report.set_payload((attach_file).read())
-        encoders.encode_base64(report)
-        #add report header with the file name
-        report.add_header("Content-Decomposition", "attachment", filename = self.file)
-        email.attach(report)
+        attach_file = open(self.file, "r+") # open the file
+        email.set_content(str(attach_file.read()))
+        attach_file.close()
         #Create SMTP session for sending the mail
         session = smtplib.SMTP_SSL('smtp.googlemail.com', 465) #use gmail with port
         session.ehlo()
@@ -41,21 +38,15 @@ class OJF:
             print('SMTPAuthenticationError')
         
         text = email.as_string()
-        session.sendmail(self.sender_email, self.receiver_email, text)
+        session.send_message(email)
         session.quit()
         print('Mail Sent')
     def createFile(self):
-        pdf = FPDF() 
+        f = open(self.file, 'r')
+        f.read()
+        f.close()
+        f = open(self.file, 'w+')
+        f.write('hello world')
+        f.close()
         
-        # Add a page 
-        pdf.add_page() 
         
-        # set style and size of font  
-        # that you want in the pdf 
-        pdf.set_font("Arial", size = 12) 
-        
-        pdf.cell(200, 10, txt = "GeeksforGeeks",  
-                    ln = 1, align = 'C') 
-            
-        # save the pdf with name .pdf 
-        pdf.output("report.pdf") 
